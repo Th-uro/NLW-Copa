184010cfd8e6ef2a71dc319c13630e9104d6d50c
Arthur Oliveira <arthurolivsira@gmail.com>
2022-11-25 21:04:58 -0300

    NLW-COPA DAY 6

--- main.js
+++ main.js
@@ -22,11 +22,17 @@ function createCard(date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML =
-  createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("switzerland", "07:00", "cameroon") +
+      createGame("portugal", "13:00", "ghana") +
+      createGame("brazil", "16:00", "serbia")
+  ) +
   createCard(
     "28/11",
     "segunda",
-    createGame("switzerland", "13:00", "brazil") +
+    createGame("korea", "10:00", "ghana") +
+      createGame("switzerland", "13:00", "brazil") +
       createGame("portugal", "16:00", "uruguai")
-  ) +
-  createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
+  ) 
